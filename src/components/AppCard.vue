8938010268f62761be17a28863276aabfa578b91
nespolisimmaco <snespoli@yahoo.com>
2023-04-20 13:14:08 +0200

    Add: converted language string into the flag of the corresponding nation

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -21,7 +21,15 @@ export default {
         <!-- Movie infos -->
         <div class="title">Title: {{ movieTitle }}</div>
         <div class="original-title">Original title: {{ movieOriginalTitle }}</div>
-        <div class="language">Language: {{ originalLanguage }}</div>
+        <div class="language">
+            Language:
+            <span v-if="originalLanguage === 'en'"><img src="../img/united-kingdom.png" alt="English"></span>
+            <span v-else-if="originalLanguage === 'it'"><img src="../img/italy.png" alt="Italian"></span>
+            <span v-else-if="originalLanguage === 'es'"><img src="../img/spain.png" alt="Spanish"></span>
+            <span v-else-if="originalLanguage === 'fr'"><img src="../img/france.png" alt="French"></span>
+            <span v-else-if="originalLanguage === 'ja'"><img src="../img/japan.png" alt="Japanese"></span>
+            <span v-else>{{ originalLanguage }}</span>
+        </div>
         <div class="vote">Vote: {{ vote }}</div>
     </div>
 </template>
@@ -30,5 +38,13 @@ export default {
 .ms_card {
     background-color: red;
     color: white;
+}
+
+.language {
+    span {
+        img {
+            width: 20px;
+        }
+    }
 }
 </style>
